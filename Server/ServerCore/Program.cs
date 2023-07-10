778b8efc1c2231e4a03937aaeab2a4bfe08d463c
dawn <dawn@buffstudio.com>
2023-07-11 01:37:30 +0900

    5_쓰레드 생성
    5_쓰레드 생성

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,7 +1,34 @@
-﻿static void MainThread()
+﻿static void MainThread(object state)
 {
-    Console.WriteLine("Hello Thread!");
+    for (int i = 0; i < 5; i++)
+        Console.WriteLine("Hello Thread!");
 }
 
-Thread t = new Thread(MainThread);
-t.Start();
+ThreadPool.SetMinThreads(1, 1);
+ThreadPool.SetMaxThreads(5, 5);
+
+for (int i = 0; i < 4; i++)
+{
+    Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning);
+    t.Start();
+}
+    
+
+for (int i = 0; i < 4; i++)
+    ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
+
+ThreadPool.QueueUserWorkItem(MainThread);
+
+//Thread t = new Thread(MainThread);
+//t.Name = "Test Thread";
+//t.IsBackground = true;
+//t.Start();
+
+//Console.WriteLine("Waiting for Thread!");
+
+//t.Join();
+//Console.WriteLine("Hello World!");
+while (true)
+{
+
+}
