81ececf1d0411db364bf76d7919c2d665770e999
dawn <bbdlek@gmail.com>
2023-07-13 02:37:38 +0900

    8. 메모리 배리어 1
    8. 메모리 배리어 1

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -4,39 +4,59 @@ namespace ServerCore
 {
     class Program
     {
+        // 메모리 배리어
+        // A) 코드 재배치 억제
+        // B) 가시성
+
+        // 1) Full Memory Barrier (Assembly -> MFENCE, C# Thread.MemoryBarrier) : Store/Load 둘다 막는다
+        // 2) Store Memory Barrier (ASM SFENCE) : Store만 막는다
+        // 3) Load Memory Barrier (ASM LFENCE) : Load만 막는다
+
+        static int x = 0;
+        static int y = 0;
+        static int r1 = 0;
+        static int r2 = 0;
+
+        static void Thread_1()
+        {
+            y = 1; // Store y
+
+            // ----------------------------
+            Thread.MemoryBarrier();
+
+            r1 = x; // Load x
+        }
+
+        static void Thread_2()
+        {
+            x = 1; // Store x
+
+            // ----------------------------
+            Thread.MemoryBarrier();
+
+            r2 = y; // Load y
+        }
+
         static void Main(string[] args)
         {
-            // 5 * 5 배열
-            // [][][][][] [][][][][] [][][][][] [][][][][] [][][][][]
-            // 첫번째 : 캐시에 미리 어느정도 담아 놓는데 공간적 이점 활용
-            // 두번째 : 띄엄띄엄 값이 들어가서 공간적 이점 활용 X
-            int[,] arr = new int[10000, 10000];
-
+            int count = 0;
+            while (true)
             {
-                long now = DateTime.Now.Ticks;
-                for (int y = 0; y < 10000; y++)
-                {
-                    for(int x = 0; x < 10000;  x++)
-                    {
-                        arr[y, x] = 1;
-                    }
-                }
-                long end = DateTime.Now.Ticks;
-                Console.WriteLine($"(y, x) 순서 걸린 시간 {end - now}");
+                count++;
+                x = y = r1 = r2 = 0;
+
+                Task t1 = new Task(Thread_1);
+                Task t2 = new Task(Thread_2);
+                t1.Start();
+                t2.Start();
+
+                Task.WaitAll(t1, t2);
+
+                if(r1 == 0 && r2 == 0)
+                    break;
             }
 
-            {
-                long now = DateTime.Now.Ticks;
-                for (int y = 0; y < 10000; y++)
-                {
-                    for (int x = 0; x < 10000; x++)
-                    {
-                        arr[x, y] = 1;
-                    }
-                }
-                long end = DateTime.Now.Ticks;
-                Console.WriteLine($"(x, y) 순서 걸린 시간 {end - now}");
-            }
+            Console.WriteLine($"{count}번만에 빠져나옴!");
         }
     }
 }
