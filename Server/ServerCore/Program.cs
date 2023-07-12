7048ae4114f7f1ab7b05f1df3f8392c8f23b6c1e
dawn <bbdlek@gmail.com>
2023-07-13 02:15:27 +0900

    7. 캐시 이론
    7. 캐시 이론

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -4,35 +4,39 @@ namespace ServerCore
 {
     class Program
     {
-        volatile static bool _stop = false; //쓰레드 동시 접근 가능
-
-        static void ThreadMain()
-        {
-            Console.WriteLine("쓰레드 시작!");
-
-            while (_stop == false)
-            {
-                //누군가가 stop 신호를 해주기를 기다린다
-            }
-
-            Console.WriteLine("쓰레드 종료!");
-        }
-
         static void Main(string[] args)
         {
-            Task t = new Task(ThreadMain);
-            t.Start();
+            // 5 * 5 배열
+            // [][][][][] [][][][][] [][][][][] [][][][][] [][][][][]
+            // 첫번째 : 캐시에 미리 어느정도 담아 놓는데 공간적 이점 활용
+            // 두번째 : 띄엄띄엄 값이 들어가서 공간적 이점 활용 X
+            int[,] arr = new int[10000, 10000];
 
-            Thread.Sleep(1000); //1s Sleep
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                {
+                    for(int x = 0; x < 10000;  x++)
+                    {
+                        arr[y, x] = 1;
+                    }
+                }
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(y, x) 순서 걸린 시간 {end - now}");
+            }
 
-            _stop = true;
-
-            Console.WriteLine("Stop 호출");
-            Console.WriteLine("종료 대기중");
-
-            t.Wait();
-
-            Console.WriteLine("종료 성공");
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                {
+                    for (int x = 0; x < 10000; x++)
+                    {
+                        arr[x, y] = 1;
+                    }
+                }
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(x, y) 순서 걸린 시간 {end - now}");
+            }
         }
     }
 }
