21e98d2c9e1887d45f8ad0ca061f079a88426c27
dawn <bbdlek@gmail.com>
2023-07-13 02:41:24 +0900

    8. 메모리 배리어 2
    8. 메모리 배리어 2

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -4,59 +4,30 @@ namespace ServerCore
 {
     class Program
     {
-        // 메모리 배리어
-        // A) 코드 재배치 억제
-        // B) 가시성
+        int _answer;
+        bool _complete;
 
-        // 1) Full Memory Barrier (Assembly -> MFENCE, C# Thread.MemoryBarrier) : Store/Load 둘다 막는다
-        // 2) Store Memory Barrier (ASM SFENCE) : Store만 막는다
-        // 3) Load Memory Barrier (ASM LFENCE) : Load만 막는다
-
-        static int x = 0;
-        static int y = 0;
-        static int r1 = 0;
-        static int r2 = 0;
-
-        static void Thread_1()
+        void A()
         {
-            y = 1; // Store y
-
-            // ----------------------------
-            Thread.MemoryBarrier();
-
-            r1 = x; // Load x
+            _answer = 123;
+            Thread.MemoryBarrier(); // Barrier 1
+            _complete = true;
+            Thread.MemoryBarrier(); // Barrier 2
         }
 
-        static void Thread_2()
+        void B()
         {
-            x = 1; // Store x
-
-            // ----------------------------
-            Thread.MemoryBarrier();
-
-            r2 = y; // Load y
+            Thread.MemoryBarrier(); // Barrier 3
+            if (_complete)
+            {
+                Thread.MemoryBarrier(); // Barrier 4
+                Console.WriteLine(_answer);
+            }
         }
 
         static void Main(string[] args)
         {
-            int count = 0;
-            while (true)
-            {
-                count++;
-                x = y = r1 = r2 = 0;
-
-                Task t1 = new Task(Thread_1);
-                Task t2 = new Task(Thread_2);
-                t1.Start();
-                t2.Start();
-
-                Task.WaitAll(t1, t2);
-
-                if(r1 == 0 && r2 == 0)
-                    break;
-            }
-
-            Console.WriteLine($"{count}번만에 빠져나옴!");
+            
         }
     }
 }
