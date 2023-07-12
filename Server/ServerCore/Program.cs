90cea85a99a4094e1f3f1527bada1ceb63d7213f
dawn <dawn@buffstudio.com>
2023-07-12 23:46:57 +0900

    6. 컴파일러 최적화
    6. 컴파일러 최적화

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,34 +1,38 @@
-﻿static void MainThread(object state)
+﻿using System;
+
+namespace ServerCore
 {
-    for (int i = 0; i < 5; i++)
-        Console.WriteLine("Hello Thread!");
-}
+    class Program
+    {
+        volatile static bool _stop = false; //쓰레드 동시 접근 가능
 
-ThreadPool.SetMinThreads(1, 1);
-ThreadPool.SetMaxThreads(5, 5);
+        static void ThreadMain()
+        {
+            Console.WriteLine("쓰레드 시작!");
 
-for (int i = 0; i < 4; i++)
-{
-    Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning);
-    t.Start();
-}
-    
+            while (_stop == false)
+            {
+                //누군가가 stop 신호를 해주기를 기다린다
+            }
 
-for (int i = 0; i < 4; i++)
-    ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
+            Console.WriteLine("쓰레드 종료!");
+        }
 
-ThreadPool.QueueUserWorkItem(MainThread);
+        static void Main(string[] args)
+        {
+            Task t = new Task(ThreadMain);
+            t.Start();
 
-//Thread t = new Thread(MainThread);
-//t.Name = "Test Thread";
-//t.IsBackground = true;
-//t.Start();
+            Thread.Sleep(1000); //1s Sleep
 
-//Console.WriteLine("Waiting for Thread!");
+            _stop = true;
 
-//t.Join();
-//Console.WriteLine("Hello World!");
-while (true)
-{
+            Console.WriteLine("Stop 호출");
+            Console.WriteLine("종료 대기중");
 
+            t.Wait();
+
+            Console.WriteLine("종료 성공");
+        }
+    }
 }
