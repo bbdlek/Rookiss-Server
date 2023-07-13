bf161a5a12fb56ce27c6c6a15a7ee1d5376f7556
dawn <bbdlek@gmail.com>
2023-07-13 16:44:54 +0900

    13. Context Switching

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -17,6 +17,11 @@
                 int desired = 1;
                 if (Interlocked.CompareExchange(ref _locked, desired, expected) == 0)
                     break;
+
+                // 쉬다 올게~
+                // Thread.Sleep(1); // 무조건 휴식 => 무조건  1ms 정도 쉬고 싶어요
+                // Thread.Sleep(0); // 조건부 양보 => 나보다 우선순위가 낮은 애들한테는 양보 불가 => 우선순위가 나보다 같거나 높은 쓰레드가 없으면 다시 본인한테
+                Thread.Yield(); // 관대한 양보 => 관대하게 양보할테니, 지금 실행이 가능한 쓰레드가 있으면 실행하세요 => 실행 가능한 애가 없으면 남은 시간 소진
             }
         }
 
