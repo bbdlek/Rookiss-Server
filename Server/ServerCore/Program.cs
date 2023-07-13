ea3ffc7b58c483796ee8bbdaf25e49c998f42bea
dawn <bbdlek@gmail.com>
2023-07-13 17:35:05 +0900

    14. AutoResetEvent
    14. AutoResetEvent

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,48 +1,21 @@
 ﻿namespace ServerCore
 {
-    class SpinLock
-    {
-        volatile int _locked = 0;
-
-        public void Acquire()
-        {
-            while (true)
-            {
-                //int original = Interlocked.Exchange(ref _locked, 1);
-                //if (original == 0)
-                //    break;
-
-                // CAS Compare-And-Swap
-                int expected = 0;
-                int desired = 1;
-                if (Interlocked.CompareExchange(ref _locked, desired, expected) == 0)
-                    break;
-
-                // 쉬다 올게~
-                // Thread.Sleep(1); // 무조건 휴식 => 무조건  1ms 정도 쉬고 싶어요
-                // Thread.Sleep(0); // 조건부 양보 => 나보다 우선순위가 낮은 애들한테는 양보 불가 => 우선순위가 나보다 같거나 높은 쓰레드가 없으면 다시 본인한테
-                Thread.Yield(); // 관대한 양보 => 관대하게 양보할테니, 지금 실행이 가능한 쓰레드가 있으면 실행하세요 => 실행 가능한 애가 없으면 남은 시간 소진
-            }
-        }
-
-        public void Release() 
-        {
-            _locked = 0;
-        }
-    }
-
-    class Program
+        class Program
     {
         static int _num = 0;
-        static SpinLock _lock = new SpinLock();
+
+        // int, ThreadId
+        static Mutex _lock = new Mutex();
 
         static void Thread_1()
         {
             for(int i = 0; i < 100000; i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
+                _lock.WaitOne();
                 _num++;
-                _lock.Release();
+                _lock.ReleaseMutex();
+                _lock.ReleaseMutex();
             }
         }
 
@@ -50,9 +23,9 @@
         {
             for (int i = 0; i < 100000; i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
                 _num--;
-                _lock.Release();
+                _lock.ReleaseMutex();
             }
         }
 
